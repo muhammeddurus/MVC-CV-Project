c77d8a590bd7eefe0b2adca4de7a6915a7e979fb
Muhammed Duruş <muhammeduruss@gmail.com>
2020-11-07 11:56:39 +0300

    project

---
+++ StaticWeb/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace StaticWeb
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
